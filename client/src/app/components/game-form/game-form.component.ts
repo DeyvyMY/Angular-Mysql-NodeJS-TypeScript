a549cf16f3d1ed1e25ec9e9bd9f73b2b23062adb
Deyvy <odr5_deivioscar@hotmail.com>
2019-03-02 23:08:20 -0500

    Tutorial Terminado

---
+++ client/src/app/components/game-form/game-form.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { Game } from 'src/app/models/Game';
+import {ActivatedRoute, Router} from '@angular/router';
+
+import {GameService} from '../../services/game.service';
+
+@Component({
+  selector: 'app-game-form',
+  templateUrl: './game-form.component.html',
+  styleUrls: ['./game-form.component.css']
+})
+export class GameFormComponent implements OnInit {
+
+  @HostBinding('class') classes = 'row';
+
+  game: Game= {
+    id:0,
+    title: '',
+    description: '',
+    image: '',
+    create_at: new Date()
+    
+  }
+
+  edit: boolean = false;
+
+  constructor(private gameService: GameService, private router: Router, private activatedRoute: ActivatedRoute) { 
+
+  }
+
+  ngOnInit() {
+    const params = this.activatedRoute.snapshot.params;
+    if(params.id){
+      this.gameService.getGame(params.id).subscribe(
+        res =>{
+          console.log(res);
+          this.game = res;
+          this.edit = true;
+        },
+        err => console.error(err)
+      )
+    }
+  }
+
+  saveNewGame(){
+    delete this.game.create_at;
+    delete this.game.id;
+    
+    this.gameService.saveGame(this.game)
+      .subscribe(
+        res => {
+          console.log(res);
+          this.router.navigate(['/games']);
+        },
+        err => console.error(err)
+      )
+  }
+
+  updateGame(){
+    delete this.game.create_at;
+    this.gameService.updateGame(this.game.id,this.game).subscribe(
+      res =>{
+        console.log(res);
+        this.router.navigate(['/games']);
+      },
+      err => console.error(err)
+    )
+  }
+
+}
